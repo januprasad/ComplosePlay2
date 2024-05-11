eaf1ff2729c9959bf0fe4668ae380341295ce2cd
Januprasad K <jnuprasad@gmail.com>
2024-05-11 16:08:54 +0530

    swapping for three buttons

--- app/src/main/java/com/github/composeplay/MainActivity.kt
+++ app/src/main/java/com/github/composeplay/MainActivity.kt
@@ -3,36 +3,24 @@ package com.github.composeplay
 import PageIndicator
 import PagerUiScreen
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntOffsetAsState
-import androidx.compose.animation.core.animateIntSizeAsState
-import androidx.compose.animation.core.animateOffsetAsState
-import androidx.compose.animation.core.animateSizeAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -40,15 +28,12 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -56,26 +41,15 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.boundsInParent
-import androidx.compose.ui.layout.boundsInRoot
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
-import androidx.compose.ui.layout.positionInRoot
-import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
 import com.github.composeplay.ui.theme.ComposePlayTheme
 import images
 import kotlinx.coroutines.launch
-import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
@@ -108,8 +82,9 @@ fun App() {
             .padding(10.dp)
 //            .border(1.dp, Color.White)
     ) {
-        var moved by remember { mutableStateOf(false) }
-        val defaultWidth = 33.dp
+        var oneToTwo by remember { mutableStateOf(false) }
+        var twoToThree by remember { mutableStateOf(false) }
+        val defaultWidth = 44.dp
         val capsuleWidth = defaultWidth.times(2)
         val padding = defaultWidth.div(2)
         val paddingPx = with(LocalDensity.current) {
@@ -121,8 +96,14 @@ fun App() {
         val pxToMoveRight = with(LocalDensity.current) {
             paddingPx.times(2).minus(pxToMoveLeft + capsuleWidth.toPx()).roundToInt()
         }
+        val pxToMoveRight1 = with(LocalDensity.current) {
+            (pxToMoveLeft + capsuleWidth.toPx()).roundToInt()
+        }
+        val pxToMoveLeft1 = with(LocalDensity.current) {
+            (pxToMoveRight - defaultWidth.toPx()).plus(paddingPx.times(2)).roundToInt()
+        }
         val offsetLeft by animateIntOffsetAsState(
-            targetValue = if (moved) {
+            targetValue = if (oneToTwo) {
                 IntOffset(pxToMoveLeft, 0)
             } else {
                 IntOffset.Zero
@@ -130,10 +111,26 @@ fun App() {
             label = "offset"
         )
         val offsetRight by animateIntOffsetAsState(
-            targetValue = if (moved) {
+            targetValue = if (oneToTwo) {
                 IntOffset(pxToMoveRight, 0)
             } else {
                 IntOffset.Zero
+            },
+            label = "offset"
+        )
+        val offsetRight1 by animateIntOffsetAsState(
+            targetValue = if (oneToTwo && twoToThree) {
+                IntOffset(pxToMoveRight1, 0)
+            } else {
+                IntOffset(pxToMoveLeft, 0)
+            },
+            label = "offset"
+        )
+        val offsetLeft1 by animateIntOffsetAsState(
+            targetValue = if (oneToTwo && twoToThree) {
+                IntOffset(pxToMoveLeft1, 0)
+            } else {
+                IntOffset(0, 0)
             },
             label = "offset"
         )
@@ -141,7 +138,7 @@ fun App() {
             modifier = Modifier
                 .padding(horizontal = padding)
                 .offset {
-                    offsetLeft
+                    if(twoToThree) offsetRight1 else offsetLeft
                 }
                 .background(Color.White, CircleShape)
                 .size(width = capsuleWidth, height = defaultWidth)
@@ -149,7 +146,10 @@ fun App() {
                     interactionSource = remember { MutableInteractionSource() },
                     indication = null
                 ) {
-                    moved = !moved
+                    if(oneToTwo&&twoToThree){
+                        twoToThree=!twoToThree
+                    }
+                    oneToTwo = !oneToTwo
                 }
         )
         Box(
@@ -164,23 +164,23 @@ fun App() {
                     interactionSource = remember { MutableInteractionSource() },
                     indication = null
                 ) {
-                    moved = !moved
+                    oneToTwo = !oneToTwo
                 }
         )
 
         Box(
             modifier = Modifier
                 .padding(horizontal = padding)
-//                .offset {
-//                    -offset.div(2.4f)
-//                }
+                .offset {
+                    offsetLeft1
+                }
                 .background(Color.Green, CircleShape)
                 .size(defaultWidth)
                 .clickable(
                     interactionSource = remember { MutableInteractionSource() },
                     indication = null
                 ) {
-                    moved = !moved
+                    twoToThree = !twoToThree
                 }
         )
     }
