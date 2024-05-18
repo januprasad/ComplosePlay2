7c2b5600adc62a1cf3b5b5104350ef0d613e7d81
Januprasad K <jnuprasad@gmail.com>
2024-05-18 21:58:39 +0530

    basic swap indicator

--- app/src/main/java/com/github/composeplay/Carousel.kt
+++ app/src/main/java/com/github/composeplay/Carousel.kt
@@ -2,6 +2,7 @@ package com.github.composeplay
 
 import android.util.Log
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.collectIsDraggedAsState
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
@@ -25,11 +29,19 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
+import images
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
@@ -41,8 +53,9 @@ fun Carousel(
     autoScrollDuration: Long = 2000L,
     autoScroll: Boolean = true
 ) {
-    val pageCount = Int.MAX_VALUE
-    val pagerState: PagerState = rememberPagerState(initialPage = pageCount/2, pageCount = { pageCount })
+//    val pageCount = Int.MAX_VALUE
+    val pagerState: PagerState =
+        rememberPagerState(initialPage = 0, pageCount = { list.size })
     val isDragged by pagerState.interactionSource.collectIsDraggedAsState()
     var currentPageKey by remember { mutableIntStateOf(0) }
     if (isDragged.not() && autoScroll) {
@@ -66,7 +79,7 @@ fun Carousel(
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         val scope = rememberCoroutineScope()
-        Box {
+        Box(contentAlignment = Alignment.Center) {
             HorizontalPager(
                 state = pagerState,
                 contentPadding = PaddingValues(
@@ -92,6 +105,13 @@ fun Carousel(
                     PagerImages(item)
                 }
             }
+            SwapDotIndicators(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomCenter),
+                count = images.size,
+                pagerState = pagerState
+            )
         }
     }
 }
@@ -107,6 +127,69 @@ fun PagerImages(item: Int) {
                 .fillMaxWidth()
                 .aspectRatio(2f)
         )
+    }
+}
+
+val PagerState.pageOffset: Float
+    get() = this.currentPage + this.currentPageOffsetFraction
+
+
+// To get scrolled offset from snap position
+fun PagerState.calculateCurrentOffsetForPage(page: Int): Float {
+    val nextPage = (currentPage + 1).mod(pageCount)
+    return (nextPage - page) + currentPageOffsetFraction
+}
+
+@Composable
+fun SwapDotIndicators(
+    modifier: Modifier,
+    count: Int,
+    pagerState: PagerState,
+) {
+    val circleSpacing = 8.dp
+    val circleSize = 20.dp
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp), contentAlignment = Alignment.Center
+    ) {
+        val width = (circleSize + circleSpacing) * count
+
+        Canvas(
+            modifier = Modifier
+                .width(width = width)
+        ) {
+            val distance = (circleSize + circleSpacing).toPx()
+
+            val dotSize = circleSize.toPx()
+
+            val yPos = center.y
+
+            repeat(count) { i ->
+                val posOffset = pagerState.currentPage + pagerState.currentPageOffsetFraction
+
+                val dotOffset = posOffset - posOffset.toInt()
+                val current = posOffset.toInt()
+                val alpha = if (i == current) 1f else 0.4f
+
+                val moveX: Float = when {
+                    i == current -> posOffset
+                    i - 1 == current -> i - dotOffset
+                    else -> i.toFloat()
+                }
+
+//                drawIndicator(moveX * distance, yPos, dotSize, alpha)
+                drawIndicator(
+                    x = moveX * distance,
+                    y = yPos,
+                    width = dotSize,
+                    height = dotSize,
+                    radius = CornerRadius(50f, 50f),
+                    active = false
+                )
+            }
+        }
     }
 }
 
@@ -128,4 +211,23 @@ fun Modifier.carouselTransition(
     )
     alpha = transformation
     scaleY = transformation
+}
+
+private fun DrawScope.drawIndicator(
+    x: Float,
+    y: Float,
+    width: Float,
+    height: Float,
+    radius: CornerRadius,
+    active: Boolean,
+) {
+    val rect = RoundRect(
+        x,
+        y - height / 2,
+        x + width,
+        y + height / 2,
+        radius
+    )
+    val path = Path().apply { addRoundRect(rect) }
+    drawPath(path = path, color = Color.White)
 }
